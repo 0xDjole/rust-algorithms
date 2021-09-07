20dce33c8d725355466a2b74a8fecfc7646ae729
Djole <djole.nikolic.priv@gmail.com>
2021-09-07 13:44:07 +0200

    ransom note

---
+++ ransom-note/src/main.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+impl Solution {
+    pub fn can_spell(magazine: Vec<char>, note: String) -> bool {
+        let mut letters: HashMap<char, usize> = HashMap::new();
+
+        for character in magazine {
+            let counter = letters.entry(character).or_insert(0);
+            *counter += 1;
+        }
+
+        println!("LETTERS {:?}", letters);
+
+        for character in note.chars() {
+            if !letters.contains_key(&character) || letters[&character] <= 0 {
+                return false;
+            }
+
+            *letters.get_mut(&character).unwrap() -= 1;
+        }
+
+        true
+    }
+}
+
+fn main() {
+    let magazine = vec!['A', 'B', 'C', 'D', 'T', 'A'];
+    let note = String::from("CATSDSAS");
+
+    let can_spell = Solution::can_spell(magazine, note);
+
+    println!("Solution is {:?}", can_spell);
+}
