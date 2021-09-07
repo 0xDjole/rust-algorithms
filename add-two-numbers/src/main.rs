68d160aa9a00fb72dcaab36c68edf1366dcea673
Djole <djole.nikolic.priv@gmail.com>
2021-09-07 23:12:50 +0200

    add 2 numbers

--- add-two-numbers/src/main.rs
+++ add-two-numbers/src/main.rs
@@ -18,15 +18,49 @@ impl Solution {
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
+        let mut l1_current = l1;
+        let mut l2_current = l2;
+        let mut head = Some(Box::new(ListNode::new(0)));
+        let mut current = head.as_mut();
+        let mut carry = 0;
+
+        while l1_current.is_some() || l2_current.is_some() {
+            let mut sum = carry;
+
+            if let Some(node) = l1_current {
+                sum += node.val;
+                l1_current = node.next;
+            }
+
+            if let Some(node) = l2_current {
+                sum += node.val;
+                l2_current = node.next;
+            }
+
+            carry = sum / 10;
+
+            if let Some(node) = current {
+                node.next = Some(Box::new(ListNode::new(sum % 10)));
+                current = node.next.as_mut();
+            }
+        }
+
+        if carry > 0 {
+            current.unwrap().next = Some(Box::new(ListNode::new(carry)))
+        }
+
+        head.unwrap().next
     }
 }
-
 fn main() {
-    let node1 = ListNode::new(2);
-    node1.next = Some(Box::new(ListNode::new(4)));
-    node1.next = Some(Box::new(ListNode::new(3)));
+    let mut node1 = ListNode::new(3);
+    node1.next = Some(Box::new(ListNode::new(6)));
+    node1.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(5)));
 
-    let node2 = ListNode::new(5);
-    node2.next = Some(Box::new(ListNode::new(6)));
+    let mut node2 = ListNode::new(2);
     node2.next = Some(Box::new(ListNode::new(4)));
+    node2.next.as_mut().unwrap().next = Some(Box::new(ListNode::new(8)));
+
+    let result = Solution::add_two_numbers(Some(Box::new(node1)), Some(Box::new(node2)));
+    println!("RESULT {:?}", result);
 }
