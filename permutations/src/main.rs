fd431fff7ee99e2d0b9e7e3ffac63f852f2de995
Djole <djole.nikolic.priv@gmail.com>
2021-09-10 14:03:29 +0200

    123 example with permutations

--- permutations/src/main.rs
+++ permutations/src/main.rs
@@ -1,12 +1,10 @@
 struct Solution {}
 
 impl Solution {
-    // [1, 2, ,3]
     pub fn permute(numbers: Vec<i32>) -> Vec<Vec<i32>> {
         Self::permute_helper(numbers, 0)
     }
 
-    // [1, 2, 3] [0]
     fn permute_helper(mut numbers: Vec<i32>, start: i32) -> Vec<Vec<i32>> {
         if start == numbers.len() as i32 - 1 {
             return vec![numbers];
@@ -25,7 +23,7 @@ impl Solution {
     }
 }
 fn main() {
-    let numbers = vec![3, 4, 5];
+    let numbers = vec![1, 2, 3];
 
     let result = Solution::permute(numbers);
 
