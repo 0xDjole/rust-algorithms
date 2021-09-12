223c2b29893b14fdc1cfc1d7eaf5e340ef210716
Djole <djole.nikolic.priv@gmail.com>
2021-09-12 20:15:36 +0200

    three unique numbers my way

--- src/three_unique_number_sort.rs
+++ src/three_unique_number_sort.rs
@@ -4,22 +4,23 @@ impl Solution {
     fn three_unique_number_sort(mut numbers: Vec<i32>) -> Vec<i32> {
         let mut high = numbers.len() - 1;
         let mut low = 0;
-        let mut index = 0;
 
-        while index <= high {
+        for index in 0..numbers.len() {
             let number: i32 = numbers[index];
 
             if number == 1 {
                 numbers.swap(index, low);
                 low += 1;
-                index += 1;
             }
 
-            if number == 2 {
-                index += 1;
-            }
+            if number == 3 && index <= high {
+                let number_high = numbers[high];
 
-            if number == 3 {
+                if number_high == 1 {
+                    numbers.swap(high, low);
+                    low += 1;
+                }
+
                 numbers.swap(index, high);
                 high -= 1;
             }
